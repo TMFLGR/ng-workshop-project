d7961d1472e51c8e73d66caf1eea7e3f04b4c337
t.flauger <t.flauger@digiply.de>
2020-02-07 21:13:40 +0100

    08-routing-basics

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { TodosComponent } from './todos/todos.component';
+import { FilteredTodoListComponent } from './filtered-todo-list/filtered-todo-list.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+    {
+        path: '',
+        component: TodosComponent
+    },
+    {
+        path: 'done',
+        component: FilteredTodoListComponent
+    }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
